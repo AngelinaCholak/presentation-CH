84fbacbb5f26d7f350a55111b6e55bd1407bd333
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2024-01-17 21:52:53 +0400

    -

--- src/pages/Shops.jsx
+++ src/pages/Shops.jsx
@@ -7,162 +7,271 @@ import {
 import { useSwipeable } from 'react-swipeable';
 import css from './Shops.module.css';
 
-import image1 from './img/img1.jpg';
-import image2 from './img/img2.jpg';
-import image3 from './img/img3.jpg';
-import image4 from './img/img4.jpg';
-import image5 from './img/img5.jpg';
-import image6 from './img/img6.jpg';
-import image7 from './img/img7.jpg';
-import image8 from './img/img8.jpg';
-import image9 from './img/img9.jpg';
-import image10 from './img/img10.jpg';
-import image11 from './img/img11.jpg';
-import image12 from './img/img12.jpg';
-import image13 from './img/img13.jpg';
-import image14 from './img/img14.jpg';
-import image15 from './img/img15.jpg';
-import image16 from './img/img16.jpg';
-import image17 from './img/img17.jpg';
+import image1 from './img/Festival-ikia.jpeg';
+import image2 from './img/festival-lulu .jpeg';
+import image3 from './img/Festival-Plaza.jpeg';
+import image4 from './img/Respublika-1.jpeg';
+import image5 from './img/Respublika-2.jpeg';
+import image6 from './img/Silpo-Vishnya.jpg';
+import image7 from './img/Megamarket-Gatne.jpeg';
+import image8 from './img/Megamarket-hodosovka.jpeg';
+import image9 from './img/no-photo.jpg';
+import image10 from './img/no-photo.jpg';
+import image11 from './img/no-photo.jpg';
+import image12 from './img/no-photo.jpg';
+import image13 from './img/no-photo.jpg';
+import image14 from './img/no-photo.jpg';
+import image15 from './img/no-photo.jpg';
 
-const images = [
+import concept1 from './img/concept1.jpg';
+import concept2 from './img/concept2.jpg';
+import concept3 from './img/concept3.jpg';
+import concept4 from './img/concept4.jpg';
+import concept5 from './img/concept5.jpg';
+import concept6 from './img/concept6.jpg';
+import concept7 from './img/concept7.jpg';
+import concept8 from './img/concept8.jpg';
+import concept9 from './img/concept9.jpg';
+//
+const images1 = [
   {
     src: image1,
-    title: 'Заголовок 1',
-    description: 'Опис 1',
+    title: 'UAE, Dubai, Festival City Mall',
+    description: 'Work time: 09:00 - 23:00 7/7',
   },
   {
     src: image2,
-    title: 'Заголовок 2',
-    description: 'Опис 2',
+    title: 'UAE, Dubai, Festival City Mall',
+    description: 'Work time: 09:00 - 23:00 7/7',
   },
   {
     src: image3,
-    title: 'Заголовок 3',
-    description: 'Опис 3',
+    title: 'UAE, Dubai, Festival Plaza Mall',
+    description: 'Work time: 09:00 - 22:00 7/7',
   },
   {
     src: image4,
-    title: 'Заголовок 4',
-    description: 'Опис 4',
+    title: 'Ukraine, Kyiv, Respublika Park Mall',
+    description: 'Work time: 10:00 - 20:00 7/7',
   },
   {
     src: image5,
-    title: 'Заголовок 5',
-    description: 'Опис 5',
+    title: 'Ukraine, Kyiv, Respublika Park Mall',
+    description: 'Work time: 10:00 - 20:00 7/7',
   },
   {
     src: image6,
-    title: 'Заголовок 6',
-    description: 'Опис 6',
+    title: 'Ukraine, Vyshneve, Silpo',
+    description:
+      'Evropeyska street, 30, Vyshneve, Kyiv region, Ukraine, 08132, Work time: 10:00 - 20:00 7/7',
   },
   {
     src: image7,
-    title: 'Заголовок 7',
-    description: 'Опис 7',
+    title: 'Ukraina, Gatne, MegaMarket',
+    description:
+      'Kyivska street, 2, Gatne, Kyiv region, Ukraine, 08160, Work time: 10:00 - 20:00 7/7',
   },
   {
     src: image8,
-    title: 'Заголовок 8',
-    description: 'Опис 8',
+    title: 'Ukraine, Khodosivka, MegaMarket',
+    description:
+      'Dniprovske shosse, 1, 16, Khodosivka, Kyiv region, Ukraine, 08173, Work time: 10:00 - 20:00 7/7',
   },
   {
     src: image9,
-    title: 'Заголовок 9',
-    description: 'Опис 9',
+    title: 'Ukraine, Vyshneve, NOVUS',
+    description:
+      'Pershotravneva Street, 26, Vyshneve, Kyiv region, Ukraine, 08132',
   },
   {
     src: image10,
-    title: 'Заголовок 10',
-    description: 'Опис 10',
+    title: 'Ukraine, Kyiv, Tochka Mall',
+    description: 'Oleksandra Dovzhenka Street, 1B, Kyiv, Ukraine, 03057',
   },
   {
     src: image11,
-    title: 'Заголовок 11',
-    description: 'Опис 11',
+    title: 'Ukraine, Kyiv, NOVUS',
+    description: 'Metrologichna Street, 19, Kyiv, Ukraine, 02000',
   },
   {
     src: image12,
-    title: 'Заголовок 12',
-    description: 'Опис 12',
+    title: 'Ukraine, Kyiv, MegaMarket',
+    description: 'Danyla Scherbakivskogo Street, 58, Kyiv, Ukraine, 04111',
   },
   {
     src: image13,
-    title: 'Заголовок 13',
-    description: 'Опис 13',
+    title: 'Ukraine, Kyiv, Cosmo Multimall',
+    description: 'Vadim Hetman Street, 6, Kyiv, Ukraine, 03057',
   },
   {
     src: image14,
-    title: 'Заголовок 14',
-    description: 'Опис 14',
+    title: 'Ukraine, Kyiv, ULTRAMARKET',
+    description: 'Surikova Street, 3a, Kyiv, Ukraine, 02000',
   },
   {
     src: image15,
-    title: 'Заголовок 15',
-    description: 'Опис 15',
+    title: 'Ukraine, Kyiv, NOVUS',
+    description: 'Georgiy Kirpy Street, 5, Kyiv, Ukraine, 02000',
+  },
+];
+//
+const images2 = [
+  {
+    src: concept1,
+    title: '',
+    description: '',
   },
   {
-    src: image16,
-    title: 'Заголовок 16',
-    description: 'Опис 16',
+    src: concept2,
+    title: '',
+    description: '',
   },
   {
-    src: image17,
-    title: 'Заголовок 17',
-    description: 'Опис 17',
+    src: concept3,
+    title: '',
+    description: '',
+  },
+  {
+    src: concept4,
+    title: '',
+    description: '',
+  },
+  {
+    src: concept5,
+    title: '',
+    description: '',
+  },
+  {
+    src: concept6,
+    title: '',
+    description: '',
+  },
+  {
+    src: concept7,
+    title: '',
+    description: '',
+  },
+  {
+    src: concept8,
+    title: '',
+    description: '',
+  },
+  {
+    src: concept9,
+    title: '',
+    description: '',
   },
 ];
 
 const Slider = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex1, setCurrentIndex1] = useState(0);
+  const [currentIndex2, setCurrentIndex2] = useState(0);
 
-  const nextSlide = () => {
-    setCurrentIndex(prevIndex =>
-      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+  const nextSlide1 = () => {
+    setCurrentIndex1(prevIndex =>
+      prevIndex === images1.length - 1 ? 0 : prevIndex + 1
     );
   };
 
-  const prevSlide = () => {
-    setCurrentIndex(prevIndex =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+  const prevSlide1 = () => {
+    setCurrentIndex1(prevIndex =>
+      prevIndex === 0 ? images1.length - 1 : prevIndex - 1
     );
   };
 
-  const handlers = useSwipeable({
-    onSwipedLeft: () => nextSlide(),
-    onSwipedRight: () => prevSlide(),
+  const nextSlide2 = () => {
+    setCurrentIndex2(prevIndex =>
+      prevIndex === images2.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const prevSlide2 = () => {
+    setCurrentIndex2(prevIndex =>
+      prevIndex === 0 ? images2.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handlers1 = useSwipeable({
+    onSwipedLeft: () => nextSlide1(),
+    onSwipedRight: () => prevSlide1(),
   });
 
-  const currentImage = images[currentIndex];
+  const handlers2 = useSwipeable({
+    onSwipedLeft: () => nextSlide2(),
+    onSwipedRight: () => prevSlide2(),
+  });
+
+  const currentImage1 = images1[currentIndex1];
+  const currentImage2 = images2[currentIndex2];
 
   return (
-    <div {...handlers} className={css.sliderContainer}>
-      <div className={css.sliderContent}>
-        <div className={css.slideContainer}>
-          <div className={css.captionContainer}>
-            <h2 className={css.title}>{currentImage.title}</h2>
+    <div>
+      <div>
+        <div {...handlers1} className={css.sliderContainer}>
+          <div className={css.sliderContent}>
+            <div className={css.slideContainer}>
+              <div className={css.captionContainer}>
+                <h2 className={css.title}>{currentImage1.title}</h2>
+              </div>
+              <img
+                src={currentImage1.src}
+                alt={currentImage1.title}
+                className={css.slide}
+                width="800"
+                height="500"
+                loading="lazy"
+              />
+              <div className={css.captionContainer}>
+                <p className={css.description}>{currentImage1.description}</p>
+              </div>
+            </div>
           </div>
-          <img
-            src={currentImage.src}
-            alt={currentImage.title}
-            className={css.slide}
-            width="800"
-            height="500"
-            loading="lazy"
-          />
-          <div className={css.captionContainer}>
-            <p className={css.description}>{currentImage.description}</p>
+
+          <div className={css.buttonContainer}>
+            <button className={`${css.arrow} ${css.prev}`} onClick={prevSlide1}>
+              <FontAwesomeIcon icon={faChevronLeft} />
+            </button>
+            <button className={`${css.arrow} ${css.next}`} onClick={nextSlide1}>
+              <FontAwesomeIcon icon={faChevronRight} />
+            </button>
           </div>
         </div>
       </div>
 
-      <div className={css.buttonContainer}>
-        <button className={`${css.arrow} ${css.prev}`} onClick={prevSlide}>
-          <FontAwesomeIcon icon={faChevronLeft} />
-        </button>
-        <button className={`${css.arrow} ${css.next}`} onClick={nextSlide}>
-          <FontAwesomeIcon icon={faChevronRight} />
-        </button>
+      <div className={css.sectionContainer}>
+        <p className={css.block}>Concept</p>
+      </div>
+
+      <div>
+        <div {...handlers2} className={css.sliderContainer}>
+          <div className={css.sliderContent}>
+            <div className={css.slideContainer}>
+              {/* <div className={css.captionContainer}>
+              <h2 className={css.title}>{currentImage.title}</h2>
+            </div> */}
+              <img
+                src={currentImage2.src}
+                alt={currentImage2.title}
+                className={css.slide}
+                width="800"
+                height="500"
+                loading="lazy"
+              />
+              {/* <div className={css.captionContainer}>
+              <p className={css.description}>{currentImage.description}</p>
+            </div> */}
+            </div>
+          </div>
+
+          <div className={css.buttonContainer}>
+            <button className={`${css.arrow} ${css.prev}`} onClick={prevSlide2}>
+              <FontAwesomeIcon icon={faChevronLeft} />
+            </button>
+            <button className={`${css.arrow} ${css.next}`} onClick={nextSlide2}>
+              <FontAwesomeIcon icon={faChevronRight} />
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/SliderCH.jsx
+++ src/pages/SliderCH.jsx
@@ -7,29 +7,106 @@ import {
 import { useSwipeable } from 'react-swipeable';
 import css from './Shops.module.css';
 
-import image1 from './img/img1.jpg';
-import image2 from './img/img2.jpg';
-import image3 from './img/img3.jpg';
+import image1 from './img/imgProducts/img1.jpg';
+import image2 from './img/imgProducts/img2.jpg';
+import image3 from './img/imgProducts/img3.jpg';
+import image4 from './img/imgProducts/img4.jpg';
+import image5 from './img/imgProducts/img5.jpg';
+import image6 from './img/imgProducts/img6.jpg';
+import image7 from './img/imgProducts/img7.jpg';
+import image8 from './img/imgProducts/img8.jpg';
+import image9 from './img/imgProducts/img9.jpg';
+import image10 from './img/imgProducts/img10.jpg';
+import image11 from './img/imgProducts/img11.jpg';
+import image12 from './img/imgProducts/img12.jpg';
+import image13 from './img/imgProducts/img13.jpg';
+import image14 from './img/imgProducts/img14.jpg';
 
 
 const images = [
   {
     src: image1,
-    title: 'Заголовок 1',
-    description: 'Опис 1',
-    price: 'Опис 1',
+    title: 'Car charger',
+    description: '',
+    price: ' 89 - 489 AED',
   },
   {
     src: image2,
-    title: 'Заголовок 2',
-    description: 'Опис 2',
-    price: 'Опис 2',
+    title: 'Magnetic wallet for case',
+    description: '',
+    price: ' 99 - 199 AED',
   },
   {
     src: image3,
-    title: 'Заголовок 3',
-    description: 'Опис 3',
-    price: 'Опис 3',
+    title: 'Strap for watch',
+    description: '',
+    price: ' 69 - 249 AED',
+  },
+  {
+    src: image4,
+    title: 'Case for phone',
+    description: 'Skinarma',
+    price: ' 149 - 249 AED',
+  },
+  {
+    src: image5,
+    title: 'Stabilazor',
+    description: '',
+    price: ' 149 - 789 AED',
+  },
+  {
+    src: image6,
+    title: 'Powerbank',
+    description: '',
+    price: ' 99 - 399 AED',
+  },
+  {
+    src: image7,
+    title: 'Screen protector',
+    description: '',
+    price: ' 50 - 220 AED',
+  },
+  {
+    src: image8,
+    title: 'Case for phone',
+    description: 'Skinarma',
+    price: ' 149 - 249 AED',
+  },
+  {
+    src: image9,
+    title: 'Tripod',
+    description: '',
+    price: ' 79 - 199 AED',
+  },
+  {
+    src: image10,
+    title: 'Home Charger',
+    description: 'Опис 10',
+    price: '69 - 349 AED',
+  },
+  {
+    src: image11,
+    title: 'Camera protector',
+    description: '',
+    price: ' 49 - 129 AED',
+  },
+  {
+    src: image12,
+    title: 'Cable',
+    description: '',
+    price: ' 39 - 189 AED',
+  },
+  {
+    src: image13,
+    title: 'Case for phone',
+    description: 'Pitaka',
+    price: ' 349 AED',
+  },
+  {
+    src: image14,
+    title: 'Case for phone',
+    description: 'Pitaka',
+    price: ' 349 AED',
   },
 ];
 
@@ -67,7 +144,7 @@ const SliderCH = () => {
             alt={currentImage.title}
             className={css.slide}
             width="800"
-            height="500"
+            height="800"
             loading="lazy"
           />
           <div className={css.price}>
